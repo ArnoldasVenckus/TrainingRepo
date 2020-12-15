a0754ad0dcaadebc6c8f991ff45bcb44bcb5fabb
Arnoldas Venckus <arnoldas.venckus@gmail.com>
2020-12-15 20:44:54 +0200

    Final push

--- 1 Praktinis/JS/main.js
+++ 1 Praktinis/JS/main.js
@@ -1,6 +1,18 @@
-var b = document.querySelector("p");
-var a = document.querySelector(".btn");
+var yellow = document.querySelector(".ylw");
+var red = document.querySelector(".rd");
+var green = document.querySelector(".grn");
+var text = document.querySelector(".text");
 
-a.addEventListener("click", function(){
-    text.classList.add("change");
-})
+
+yellow.addEventListener("click", function(){
+	text.classList.add("yellow");
+});
+
+
+red.addEventListener("click", function(){
+	text.classList.add("red");
+});
+
+green.addEventListener("click", function(){
+	text.classList.add("green");
+});
