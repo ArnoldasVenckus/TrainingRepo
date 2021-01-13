06250af9a0987a788b84e95c727d7ea417e7e64d
Arnoldas Venckus <arnoldas.venckus@gmail.com>
2021-01-13 10:24:52 +0200

    1

---
+++ Events and Forms/Praktinis 1/Files/main.js
@@ -0,0 +1,26 @@
+let rand = Math.floor(Math.random() * 101);
+let form = document.querySelector('[name=form]');
+let result = document.querySelector('#resultText');
+let resultCount = document.querySelector('#resultCount');
+let count = 0;
+
+form.addEventListener('submit', run);
+
+function run(e){
+    e.preventDefault();
+    let guess = e.target.elements.input.value;
+    if (guess == rand){
+        result.innerHTML = "Bullseye!";
+    }
+    else if(guess > rand){
+        result.innerHTML = "Skaicius yra mazesnis";
+    }
+    else if(guess < rand){
+        result.innerHTML = "Skaicius yra didesnis";
+    }
+    count++;
+    resultCount.innerHTML = "Bandymai: " + count;
+}
+
+
+console.log(rand);
